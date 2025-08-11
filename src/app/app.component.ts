def586604ac66a77400998e5b7e25f5d4f72e45b
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-11 11:59:32 +0530

    refactor: update app component to use LogComponent and enhance log display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LogComponent } from './components/log/log.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/log/log.component.ts
+++ src/app/components/log/log.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { LogEntry, LogService } from '../../services/log.service';
 
 @Component({
   selector: 'app-log',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './log.component.html',
   styleUrl: './log.component.css'
 })
-export class LogComponent {
+export class LogComponent implements OnInit {
+  logs: LogEntry[] = [];
+
+  constructor(private logService: LogService) {}
+
+  ngOnInit() {
+    this.logService.getLogs().subscribe(data => {
+      this.logs = data;
+    });
+  }
 
 }
