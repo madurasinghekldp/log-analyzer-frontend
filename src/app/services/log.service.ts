2b5287ca8a4cfc92bc614de07365bda0d084bf1b
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-11 10:53:48 +0530

    implement LogService with getLogs method and LogEntry interface

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface LogEntry {
+  id: string;
+  timestamp: string;
+  level: string;
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/logs';
+
+  constructor(private http: HttpClient) {}
+
+  getLogs(): Observable<LogEntry[]> {
+    return this.http.get<LogEntry[]>(this.apiUrl);
+  }
 }
