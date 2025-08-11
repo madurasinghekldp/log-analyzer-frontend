ff0486d16082b3f06ebc0ea845735df12f81ea63
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-11 23:18:05 +0530

    feat: enhance log component with Angular Material table and loading spinner

--- src/app/components/log/log.component.ts
+++ src/app/components/log/log.component.ts
@@ -1,22 +1,25 @@
-import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { LogEntry, LogService } from '../../services/log.service';
+import { MaterialModule } from '../../material/material.module';
 
 @Component({
   selector: 'app-log',
-  imports: [NgFor],
+  imports: [NgIf,MaterialModule,],
   templateUrl: './log.component.html',
   styleUrl: './log.component.css'
 })
 export class LogComponent implements OnInit {
-  logs: LogEntry[] = [];
+  displayedColumns: string[] = ['timestamp', 'level', 'message'];
+  dataSource: LogEntry[] = [];
+  loading = true;
 
   constructor(private logService: LogService) {}
 
   ngOnInit() {
     this.logService.getLogs().subscribe(data => {
-      this.logs = data;
+      this.dataSource = data;
+      this.loading = false;
     });
   }
-
 }
